50d44ba1bd71d57d440adc88d207eb4ee8622961
Mark Menard <mark@mjm.net>
2011-08-01 14:56:38 -0400

    Add more logging around message processing.

--- lib/consumption_junction/message_processor.rb
+++ lib/consumption_junction/message_processor.rb
@@ -4,19 +4,30 @@ class ConsumptionJunction::MessageProcessor
   attr_accessor :message_count, :worker_class
   
   def initialize (worker_class)
-    puts "Create MessageProcessor for #{worker_class}"
+    log "CREATE MessageProcessor for #{worker_class}"
     self.worker_class = worker_class
     self.message_count = 0
   end
   
   def process_message (message)
-    puts "MessageProcessor#process_message #{message}"
+    log "-+" * 25
+    log "BEGIN MessageProcessor#process_message #{message}"
     self.message_count = message_count + 1
-    puts "MessageProcessor #{self} count = #{message_count}"
+    log "INFO MessageProcessor #{self} count = #{message_count}"
     build_worker.process_message(message)
+    log "END MessageProcessor#process_message #{message}"
+    log "-+" * 25
   end
   
   def build_worker
     worker_class.new
+  end
+  
+  def log (message)
+    if defined? ::Rails
+      ::Rails.logger.debug message
+    else
+      puts message
+    end
   end
 end
