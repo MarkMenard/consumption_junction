ef81c3f9dfe80bd4b101990d2c060df5935a5fe6
Mark Menard <mark@mjm.net>
2011-08-02 14:42:19 -0400

    Add some debug statements.

--- lib/consumption_junction/message_processor.rb
+++ lib/consumption_junction/message_processor.rb
@@ -4,20 +4,20 @@ class ConsumptionJunction::MessageProcessor
   attr_accessor :message_count, :worker_class
   
   def initialize (worker_class)
-    log "[ ConsumptionJunction::MessageProcessor ] CREATE MessageProcessor for #{worker_class}"
+    puts "[ ConsumptionJunction::MessageProcessor ] CREATE MessageProcessor for #{worker_class}"
     self.worker_class = worker_class
     self.message_count = 0
-    log "[ ConsumptionJunction::MessageProcessor ] END initialize()"
+    puts "[ ConsumptionJunction::MessageProcessor ] END initialize()"
   end
   
   def process_message (message)
-    log "-+" * 25
-    log "[ ConsumptionJunction::MessageProcessor ] BEGIN MessageProcessor#process_message #{message}"
+    puts "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+    puts "[ ConsumptionJunction::MessageProcessor ] BEGIN MessageProcessor#process_message #{message}"
     self.message_count = message_count + 1
-    log "[ ConsumptionJunction::MessageProcessor ] INFO MessageProcessor #{self} count = #{message_count}"
+    puts "[ ConsumptionJunction::MessageProcessor ] INFO MessageProcessor #{self} count = #{message_count}"
     build_worker.process_message(message)
-    log "[ ConsumptionJunction::MessageProcessor ] END MessageProcessor#process_message #{message}"
-    log "-+" * 25
+    puts "[ ConsumptionJunction::MessageProcessor ] END MessageProcessor#process_message #{message}"
+    puts "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
   end
   
   def build_worker
