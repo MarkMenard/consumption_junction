45584ee5f1a27a70693706f151dff2167aefb0ab
Mark Menard <mark@mjm.net>
2011-11-27 13:49:17 -0500

    Remove puts statements.

--- lib/consumption_junction/message_processor.rb
+++ lib/consumption_junction/message_processor.rb
@@ -4,16 +4,13 @@ class ConsumptionJunction::MessageProcessor
   attr_accessor :message_count, :worker_class
   
   def initialize (worker_class)
-    puts "[ ConsumptionJunction::MessageProcessor ] CREATE MessageProcessor for #{worker_class}"
     self.worker_class = worker_class
     self.message_count = 0
   end
   
   def process_message (message)
-    puts "[ ConsumptionJunction::MessageProcessor ] BEGIN MessageProcessor#process_message using #{worker_class.to_s} #{message}"
     self.message_count = message_count + 1
     result = build_worker(message).process_message
-    puts "[ ConsumptionJunction::MessageProcessor ] END MessageProcessor#process_message result = '#{result}' for message = '#{message}'"
     result
   end
   
