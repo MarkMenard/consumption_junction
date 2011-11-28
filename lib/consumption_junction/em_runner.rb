79cbef60956c4924adad5de75642fc27bf99fc4f
Mark Menard <mark@mjm.net>
2011-11-27 22:51:50 -0500

    Get clean shutdown of workers working on a SIGINT.

--- lib/consumption_junction/em_runner.rb
+++ lib/consumption_junction/em_runner.rb
@@ -2,11 +2,12 @@ module ConsumptionJunction
   class EmRunner
     include Celluloid
 
-    attr_accessor :server_config
+    attr_accessor :server_config, :subscriptions
 
     def initialize (server_config)
       @server_config = server_config
       @subscriptions = []
+      trap("SIGINT") { stop }
     end
 
     def start
@@ -14,28 +15,50 @@ module ConsumptionJunction
         create_subscriptions
       end
     end
-
-    def amqp_connection
-      @amqp_connection ||= AMQP.connect(:host => '127.0.0.1')
+    
+    def stop
+      puts "Caught SIGNINT"
+      puts "Shutting down subscriptions"
+      puts "busy_worker_count = #{busy_worker_count}"
+      EventMachine.next_tick(&method(:cancel_subscriptions))
     end
     
-    def channel
-      return @channel if @channel
-      @channel = AMQP::Channel.new(amqp_connection)
-      @channel
+    def cancel_subscriptions
+      @subscriptions.each do |subscription|
+        subscription.cancel
+      end
+      EventMachine.add_periodic_timer(0.1, &method(:stop_event_loop))
     end
-
+    
+    def stop_event_loop
+      busy_worker_count = self.busy_worker_count
+      if busy_worker_count == 0
+        puts "All jobs finished shutting down EventMachine event loop."
+        EventMachine.stop_event_loop
+      else
+        puts "There are still #{busy_worker_count} jobs processing."
+      end
+    end
+    
+    def busy_worker_count
+      @subscriptions.find_all { |subscription| subscription.job_state == EmWorkerSubscription::BUSY }.size
+    end
+    
     def create_subscriptions
       worker_configs.each do |worker_config|
         puts "Setting up subcriptions for #{worker_config}"
         worker_config.worker_count.times do
-          EmWorkerSubscription.create_subscription(channel, worker_config)
+          @subscriptions << EmWorkerSubscription.create_subscription(amqp_connection, worker_config)
         end
       end
 
       puts "END: #create_subscriptions"
     end
 
+    def amqp_connection
+      @amqp_connection ||= AMQP.connect(:host => server_config.server_host)
+    end
+    
     private
 
     def worker_configs
