a591ff5b20233d77e0a0c7e2d7ec7223f94eb4e5
Mark Menard <mark@mjm.net>
2011-11-27 22:51:30 -0500

    Add job_state tracking to the subscription.

--- lib/consumption_junction/em_worker_subscription.rb
+++ lib/consumption_junction/em_worker_subscription.rb
@@ -1,52 +1,91 @@
 class ConsumptionJunction::EmWorkerSubscription
   
-  attr_accessor :channel, :worker_config
+  IDLE = 'IDLE'
+  BUSY = 'BUSY'
+  ACTIVE = 'ACTIVE'
+  CANCELLED = 'CANCELLED'
   
-  def self.create_subscription (channel, worker_config)
-    subscription = self.new(channel, worker_config)
+  attr_reader :amqp_connection, :worker_config
+  
+  def self.create_subscription (amqp_connection, worker_config)
+    subscription = ConsumptionJunction::EmWorkerSubscription.new(amqp_connection, worker_config)
     subscription.create_subscription
     subscription
   end
   
-  def initialize (channel, worker_config)
-    @channel = channel
+  def initialize (amqp_connection, worker_config)
+    @amqp_connection = amqp_connection
     @worker_config = worker_config
+    @job_state_mutex = Mutex.new
+    @job_state = IDLE
+    @subscription_state_mutex = Mutex.new
   end
   
   def create_subscription
-    consumer.consume.on_delivery do |header, payload|
-      EventMachine.defer(operation(payload), callback(header))
+    @subscription_state_mutex.synchronize do 
+      queue.subscribe(:ack => worker_config.ack, &method(:handle_message))
+      @subscription_state = ACTIVE 
     end
   end
   
-  def consumer
-    @consumer ||= AMQP::Consumer.new(channel, queue) # , '', false, !worker_config.ack)
-  end
-
-  def queue
-    @queue ||= channel.queue(worker_config.queue, :durable => true)
+  def cancel
+    @subscription_state_mutex.synchronize do
+      queue.unsubscribe
+      @subscription_state = CANCELLED
+    end
   end
   
-  def message_processor_supervisor
-    @message_processor_supervisor ||= ConsumptionJunction::MessageProcessor.supervise(worker_config.worker_class)
+  def handle_message (header, payload)
+    EventMachine.defer(operation(payload), callback(header))
   end
   
   def operation (payload)
     lambda do
+      result = ''
+      @job_state_mutex.synchronize { @job_state = BUSY }
       begin
-        message_processor_supervisor.actor.process_message(payload)
-      rescue Exception => e
+        result = message_processor_actor.process_message(payload)
+      rescue StandardError => e
         puts e.message
         puts e.backtrace
-        "FAILED"
+        result = "FAILED"
       end
+      result
     end
   end
   
   def callback (header)
     lambda do |result|
       result == "SUCCEEDED" ? header.ack : header.reject(:requeue => worker_config.requeue_on_failure)
+      @job_state_mutex.synchronize { @job_state = IDLE }
     end
+  end
+  
+  
+  def message_processor_supervisor
+    @message_processor_supervisor ||= ConsumptionJunction::MessageProcessor.supervise(worker_config.worker_class)
+  end
+  
+  def message_processor_actor
+    message_processor_supervisor.actor
+  end
+  
+  def job_state
+    @job_state_mutex.synchronize { @job_state }
+  end
+  
+  def subscription_state
+    @subscription_state_mutex.synchronize { @subscription_state }
+  end
+  
+  def channel
+    @channel ||= AMQP::Channel.new(amqp_connection)
+    @channel.prefetch(1)
+    @channel
+  end
+  
+  def queue
+    @queue ||= channel.queue(worker_config.queue, :durable => true)
   end
 
 end
--- spec/em_worker_subscription_spec.rb
+++ spec/em_worker_subscription_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
-describe ConsumptionJunction::EmWorkerSubscription do
-  
+module ConsumptionJunction
+  describe EmWorkerSubscription do
+    let(:amqp_connection) { mock }
+    let(:worker_config) { mock }
+    
+    describe "#create_subscription" do
+      it "creates an instance with an active subscription" do
+        subscription_instance = mock()
+        subscription_instance.should_receive(:create_subscription)
+        EmWorkerSubscription.should_receive(:new).with(amqp_connection, worker_config).and_return(subscription_instance)
+        EmWorkerSubscription.create_subscription(amqp_connection, worker_config).should == subscription_instance
+      end
+    end
+    
+    describe "executing the operation" do
+      it "sets the job_state to ACTIVE" do
+        actor = mock
+        actor.stub!(:process_message)
+        
+        subscription = EmWorkerSubscription.new(amqp_connection, worker_config)
+        subscription.stub!(:message_processor_actor).and_return(actor)
+        subscription.operation("").call
+        subscription.job_state.should == EmWorkerSubscription::BUSY
+      end
+    end
+  end
 end
